e249d4ab47b406e1a0ab2d76f0fffe37cdfc818e
Lukas Hoellein <lukashoellein@gmail.com>
2020-12-30 17:03:04 +0100

    add vector transform

--- src/renderer/UVRenderer.py
+++ src/renderer/UVRenderer.py
@@ -32,9 +32,18 @@ class UVRenderer(RendererInterface):
         emission_node = nodes.new(type='ShaderNodeEmission')
         output = Utility.get_the_one_node_with_type(nodes, 'OutputMaterial')
 
+        vector_transform_node = nodes.new(type='ShaderNodeVectorTransform')
+        vector_transform_node.vector_type = "VECTOR"
+        vector_transform_node.convert_from = "OBJECT"
+        vector_transform_node.convert_to = "CAMERA"
+
+        links.new(texture_coord_node.outputs['UV'], vector_transform_node.inputs['Vector'])
+        links.new(vector_transform_node.outputs['Vector'], output.inputs['Surface'])
+
         #links.new(texture_coord_node.outputs['UV'], emission_node.inputs['Color'])
         #links.new(emission_node.outputs['Emission'], output.inputs['Surface'])
-        links.new(texture_coord_node.outputs['UV'], output.inputs['Surface'])
+
+        #links.new(texture_coord_node.outputs['UV'], output.inputs['Surface'])
         return new_mat
 
     def run(self):
