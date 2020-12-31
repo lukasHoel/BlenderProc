fe9e1159edb1949aceda79a287cfbd8aaf0d2c5e
Lukas Hoellein <lukashoellein@gmail.com>
2020-12-31 17:45:02 +0100

    save objects to file

--- src/writer/SceneWriter.py
+++ src/writer/SceneWriter.py
@@ -3,6 +3,7 @@ import bpy
 from src.utility.CameraUtility import CameraUtility
 from src.utility.ItemWriter import ItemWriter
 from src.writer.WriterInterface import WriterInterface
+from src.utility.BlenderUtility import get_all_mesh_objects
 
 import os
 
@@ -17,8 +18,14 @@ class SceneWriter(WriterInterface):
     def run(self):
         """ Collect camera and camera object and write them to a file."""
 
-        path = os.path.join(self._determine_output_dir(False), "scene.ply")
+        # save all object names
+        path = os.path.join(self._determine_output_dir(False), "objects.txt")
+        with open(path, "w") as f:
+            for obj in get_all_mesh_objects():
+                f.write(f"{obj.name}\n")
 
+        # save mesh
+        path = os.path.join(self._determine_output_dir(False), "scene.ply")
         bpy.ops.export_mesh.ply(filepath=path,
                                 use_ascii=False,
                                 use_selection=False,
