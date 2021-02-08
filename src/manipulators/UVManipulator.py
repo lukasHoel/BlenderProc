3c4a803e23e0c35ff42b36f7134ae58e0cbf4159
Lukas Hoellein <lukas.hoellein@tum.de>
2021-02-08 18:23:46 +0100

    new impl for uvmanipulator (WIP)

--- src/manipulators/UVManipulator.py
+++ src/manipulators/UVManipulator.py
@@ -8,16 +8,21 @@ class UVManipulator(Module):
         Module.__init__(self, config)
 
     def run(self):
-        objects = bpy.context.scene.objects
+        # Deselect all objects
+        bpy.ops.object.select_all(action='DESELECT')
 
-        for obj in objects:
-            obj.select_set(True)
+        # looking through all objects
+        for obj in bpy.data.objects:
+            # if the object is a mesh and not a lamp or camera etc.
+            if obj.type == 'MESH':
+                obj.select_set(True)
+                print("select", obj.name)
 
         # entering edit mode
-        #bpy.ops.object.editmode_toggle()
+        # bpy.ops.object.editmode_toggle()
 
-        #bpy.ops.mesh.select_all(action='SELECT')
+        # bpy.ops.mesh.select_all(action='SELECT')
         bpy.ops.uv.smart_project(correct_aspect=False, angle_limit=1.2217)
 
         # exiting edit mode
-        #bpy.ops.object.editmode_toggle()
+        # bpy.ops.object.editmode_toggle()
